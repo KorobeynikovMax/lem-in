4b4780b9fb1ec7d50528e80ec0283791fb62c919
Wadd Anton <wanton@student.21-school.ru>
2020-09-03 14:03:03 +0300

    scheme parse functions

--- src/main.c
+++ src/main.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "includes/lem_in.h"
+#include "lem_in.h"
 
 void			init_lem(t_lem **lem)
 {
@@ -25,7 +25,7 @@ int				main()
 	t_lem 		*lem;
 
 	init_lem(&lem);
-	parse(lem);
+	parseMap(lem);
 
 	printf("it works!");
 	return (0);
