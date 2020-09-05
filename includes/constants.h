f43dcc7d30c53a0c9ff2c4df535b8a2cf1e02a10
Wadd Anton <wanton@student.21-school.ru>
2020-09-05 19:53:45 +0300

    change line separator

--- includes/constants.h
+++ includes/constants.h
@@ -1,1 +1,11 @@
-#ifndef CONSTANTS_H# define CONSTANTS_H# define DESCRIPTOR 0# define SEPARATOR_STRSPLIT ' '# define START_ROOM_MODIFIER 1# define END_ROOM_MODIFIER 2#endif
+#ifndef CONSTANTS_H
+# define CONSTANTS_H
+
+# define DESCRIPTOR 0
+
+# define SEPARATOR_STRSPLIT ' '
+
+# define START_ROOM_MODIFIER 1
+# define END_ROOM_MODIFIER 2
+
+#endif
--- src/protecred_lib_functions.c
+++ src/protecred_lib_functions.c
@@ -1,1 +1,29 @@
-/* ************************************************************************** *//*                                                                            *//*                                                        :::      ::::::::   *//*   protecred_lib_functions.c                          :+:      :+:    :+:   *//*                                                    +:+ +:+         +:+     *//*   By: wanton <wanton@student.42.fr>              +#+  +:+       +#+        *//*                                                +#+#+#+#+#+   +#+           *//*   Created: 2020/09/05 17:51:16 by wanton            #+#    #+#             *//*   Updated: 2020/09/05 18:33:08 by wanton           ###   ########.fr       *//*                                                                            *//* ************************************************************************** */#include "lem_in.h"void		pft_get_next_line(const int fd, char **line){	if (ft_get_next_line(fd, line) == -1)		exit_with_read_error();}void		pft_str_split(char ***buff, char *line){	char	**res;	res = ft_strsplit(line, SEPARATOR_STRSPLIT);	if (res == NULL)		exit_with_malloc_error();	(*buff) = res;}
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   protecred_lib_functions.c                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wanton <wanton@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/09/05 19:47:57 by wanton            #+#    #+#             */
+/*   Updated: 2020/09/05 19:50:44 by wanton           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "lem_in.h"
+
+void		pft_get_next_line(const int fd, char **line)
+{
+	if (ft_get_next_line(fd, line) == -1)
+		exit_with_read_error();
+}
+
+void		pft_str_split(char ***buff, char *line)
+{
+	char	**res;
+
+	res = ft_strsplit(line, SEPARATOR_STRSPLIT);
+	if (res == NULL)
+		exit_with_malloc_error();
+	(*buff) = res;
+}
