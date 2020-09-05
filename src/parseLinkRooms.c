848d8d8cea42606423a3df49ea43992c6df65cd8
Benjen Davis <bedavis@il-k5.msk.21-school.ru>
2020-09-05 14:39:44 +0300

    linkparser ready

--- src/parseLinkRooms.c
+++ src/parseLinkRooms.c
@@ -12,10 +12,10 @@
 
 #include "lem_in.h"
 
-int     isValidLink(char *line, char **r1, char **r2)
+int                 isValidLink(char *line, char **r1, char **r2)
 {
-    char		**split;
-    char        *p;
+    char		    **split;
+    char            *p;
 
     if (*line == '#')
         return (1);
@@ -39,23 +39,71 @@ int     isValidLink(char *line, char **r1, char **r2)
     return (1);
 }
 
-void	parseLinksRooms(t_lem *lem, char *line)
+/*
+** return the room with name r1 or NULL
+*/
+
+t_room              *findRoom(t_lem *lem, char *room)
 {
-    char		*str;
-    char        *r1;
-    char        *r2;
+    t_room          *tmp;
+
+    tmp = lem->map;
+    if (tmp == NULL)
+        return (NULL);
+    while (!(ft_strcmp(tmp->name, room) == 0)) {
+        if (tmp->next == NULL)
+            return (NULL);
+        tmp = tmp->next;
+    }
+    return (tmp);
+}
+
+/*
+** create links in room1 and room2
+*/
+
+static void         addLink(t_lem *lem, char *r1, char *r2)
+{
+    t_room          *p1;
+    t_room          *p2;
+    t_room_address  *a;
+    t_room_address  *b;
+    t_room_address  *tmp;
+
+    p1 = findRoom(lem,r1);
+    p2 = findRoom(lem, r2);
+    if (p1 == NULL || p2 == NULL || p1 == p2)
+        return;
+    a = (t_room_address *)malloc(sizeof(t_room_address));
+    a->address = p2;
+    a->next = NULL;
+    tmp = p1->links;
+    p1->links = a;
+    p1->links->next = tmp;
+    b = (t_room_address *)malloc(sizeof(t_room_address));
+    b->address = p1;
+    b->next = NULL;
+    tmp = p2->links;
+    p2->links = b;
+    p2->links->next = tmp;
+}
+
+void	            parseLinksRooms(t_lem *lem, char *line)
+{
+    char            *r1;
+    char            *r2;
 
     if (!isValidLink(line, &r1, &r2))
         exitWithNotValidMap();
-    while (ft_get_next_line(0, &str))
+    while (1)
     {
-        if (!isValidLink(str, &r1, &r2))
+        if (line[0] == '\0')
+            break;
+        if (!isValidLink(line, &r1, &r2))
             exitWithNotValidMap();
         else
-        {
-
-        }
+            addLink(lem, r1, r2);
+        if (ft_get_next_line(0, &line) == -1)
+            exitWithReadError();
     }
-
-
 }
